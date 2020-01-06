01de8e413ed1d3b3fbdfa2d2e3c00d0d3923893a
jhadev <joshappeldev@gmail.com>
2020-01-06 13:23:29 -0500

    conditional switch for theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
 import Modern from './components/Modern';
+import Retro from './components/Retro';
+import { Button } from '@chakra-ui/core';
 
 const App = () => {
+  const [isRetro, setUI] = useState(false);
   return (
     <>
-      <Modern />
+      <Button
+        p={3}
+        variantColor="grey"
+        height="50px"
+        rounded="lg"
+        fontSize="xl"
+        width="100%"
+        mx="auto"
+        mb={2}
+        onClick={() => setUI(prevUI => !prevUI)}>
+        {isRetro ? 'modern' : 'retro'}
+      </Button>
+      {!isRetro ? <Modern /> : <Retro />}
     </>
   );
 };
