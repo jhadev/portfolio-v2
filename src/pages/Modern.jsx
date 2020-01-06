b4ecb8b3eb21408197566a61642535b0d1b02025
jhadev <joshappeldev@gmail.com>
2020-01-06 14:09:22 -0500

    create grid and map over projects

--- src/pages/Modern.jsx
+++ src/pages/Modern.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { CSSReset, SimpleGrid, Box, Image } from '@chakra-ui/core';
+import Container from '../components/Container';
 import ColorMode from '../components/ColorMode';
-import { CSSReset } from '@chakra-ui/core';
 import NavDrawer from '../components/NavDrawer';
 import ToggleColorMode from '../components/ToggleColorMode';
+import projects from '../projects.json';
 
 const Modern = () => {
   return (
@@ -12,6 +14,26 @@ const Modern = () => {
         <NavDrawer>
           <ToggleColorMode />
         </NavDrawer>
+      </div>
+      <div>
+        <Container>
+          <SimpleGrid minChildWidth="200px" spacing="2px">
+            {projects.map(project => {
+              // finish map change images props
+              return (
+                <Box bg="tomato" height="200px" key={project.id}>
+                  <Image
+                    size="200px"
+                    objectFit="cover"
+                    objectPosition="5% 5%"
+                    src={project.image}
+                    alt={project.name}
+                  />
+                </Box>
+              );
+            })}
+          </SimpleGrid>
+        </Container>
       </div>
     </ColorMode>
   );
