0e5b030e82d33ece6d20080731f1821b5d521081
jhadev <joshappeldev@gmail.com>
2020-01-06 13:34:59 -0500

    fix folder structure

--- src/pages/Modern.jsx
+++ src/pages/Modern.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ColorMode from './ColorMode';
+import ColorMode from '../components/ColorMode';
 import { CSSReset } from '@chakra-ui/core';
-import NavDrawer from './NavDrawer';
-import ToggleColorMode from './ToggleColorMode';
+import NavDrawer from '../components/NavDrawer';
+import ToggleColorMode from '../components/ToggleColorMode';
 
 const Modern = () => {
   return (
